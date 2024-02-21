ff0383984e2fc4767d0cf7e3dd405e8f0cd7f5e4
nadavdrori <nadavdrori@mail.tau.ac.il>
2024-02-21 16:53:45 +0200

    step two

--- udp_pinger.py
+++ udp_pinger.py
@@ -5,7 +5,7 @@ import sys
 client_soc = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
 # Server addres
-server_ip = '1.2.3.4'
+server_ip = sys.argv[0]
 
 server_port = 1337
 data_size = 100
@@ -13,20 +13,26 @@ msg_count = 10
 wait_timeout = 1000
 
 if '-p' in sys.argv:
-    server_port = sys.argv[sys.argv.index('-p') + 1]
+    server_port = sys.argv[(sys.argv).index('-p') + 1]
 if '-s' in sys.argv:
-    data_size = sys.argv[sys.argv.index('-s') + 1]
+    data_size = sys.argv[(sys.argv).index('-s') + 1]
 if '-c' in sys.argv:
-    msg_count = sys.argv[sys.argv.index('-c') + 1]
+    msg_count = sys.argv[(sys.argv).index('-c') + 1]
 if '-t' in sys.argv:
-    wait_timeout = sys.argv[sys.argv.index('-t') + 1]
+    wait_timeout = sys.argv[(sys.argv).index('-t') + 1]
 
 server_add = (server_ip, server_port)
 
-message = ""
+for i in range(msg_count):
+    msg_opcode = my_byte = bytes([0])
+    msg_id = i
+    msg_data = ""
 
-client_soc.sendto(message, server_add)
+    message = msg_opcode+msg_id+msg_data
 
-data, server = client_soc.recvfrom(1024)
+    client_soc.sendto(message, server_add)
+    data, server = client_soc.recvfrom(1024)
+
+    print("64 bytes from ", server_ip ," : seq= ", i ," rtt= ", tim)
 
 client_soc.close()
