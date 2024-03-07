e20413b3b470e11d6352f6be3e2790123f768ff8
nadavdrori <nadavdrori@mail.tau.ac.il>
2024-03-07 15:34:34 +0200

    finished project

--- udp_agent.py
+++ udp_agent.py
@@ -1,19 +1,28 @@
 import socket
 import sys
+import select
 
+try:
+    # Create a UDP socket
+    server_soc = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
+    # Local address to bind to
+    host = "127.0.0.1"
+    port = 1337
+    if len(sys.argv) == 3:
+        port = int(sys.argv[2])
 
-# Creat a UDP socket
-server_soc = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    # Bind to the port
+    server_soc.bind((host, port))
 
-# Local addres to bind to
-host = "127.0.0.1"
-port = 1337
-if sys.argc == 3:
-    port = sys.argv[2]
+    # Receive message
+    while True:
+        readable, _, _ = select.select([server_soc],[],[])
+        for sock in readable: 
+            data, addr = sock.recvfrom(1400)
+            msg = list(data)
+            msg[0] = 1
+            sock.sendto(bytes(msg), addr)
 
-# Bind to the port
-server_soc.bind((host, port))
-
-# Receive message
-data, addr = server_soc.recvfrom(1024)
+except OSError as exception:
+    print(exception.strerror)
--- udp_pinger.py
+++ udp_pinger.py
@@ -1,38 +1,53 @@
 import socket
 import sys
+import datetime
 
-# Creat a UDP socket
-client_soc = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+try:
+    # Create a UDP socket
+    client_soc = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-# Server addres
-server_ip = sys.argv[0]
+    # Server address
+    server_ip = sys.argv[1]
 
-server_port = 1337
-data_size = 100
-msg_count = 10
-wait_timeout = 1000
+    server_port = 1337
+    data_size = 100
+    msg_total = 10
+    wait_timeout = 1000.0
 
-if '-p' in sys.argv:
-    server_port = sys.argv[(sys.argv).index('-p') + 1]
-if '-s' in sys.argv:
-    data_size = sys.argv[(sys.argv).index('-s') + 1]
-if '-c' in sys.argv:
-    msg_count = sys.argv[(sys.argv).index('-c') + 1]
-if '-t' in sys.argv:
-    wait_timeout = sys.argv[(sys.argv).index('-t') + 1]
+    if '-p' in sys.argv:
+        server_port = int(sys.argv[(sys.argv).index('-p') + 1])
+    if '-s' in sys.argv:
+        data_size = int(sys.argv[(sys.argv).index('-s') + 1])
+    if '-c' in sys.argv:
+        msg_total = int(sys.argv[(sys.argv).index('-c') + 1])
+    if '-t' in sys.argv:
+        wait_timeout = float(sys.argv[(sys.argv).index('-t') + 1])
 
-server_add = (server_ip, server_port)
+    server_add = (server_ip, server_port)
+    client_soc.settimeout(wait_timeout/1000)
+    msg_count = 0
+    for i in range(msg_total):
+        # sending msg
+        temp = 0
+        msg_opcode = temp.to_bytes(1, 'big')
+        msg_id = i.to_bytes(4, 'big')
+        msg_data = temp.to_bytes(data_size, 'big')
+        msg = list(msg_opcode) + list(msg_id) + list(msg_data)
+        client_soc.sendto(bytes(msg), server_add)
+        # waiting for response till timeout
+        try:
+            a = datetime.datetime.now()
+            data, server = client_soc.recvfrom(data_size + 4 + 1)
+            b = datetime.datetime.now()
+            c = b - a
+            print(f"{len(data)} bytes from {server_ip}: seq={i} rtt={c.seconds() / 1000} ms")
+            msg_count += 1
+        except TimeoutError:
+            print("request timeout for icmp_seq", i)
+            
+    print(f"--- {server_ip} statistics ---")
+    print(f"{msg_total} packets transmitted, {msg_count} packets received, {(((1-(msg_count/msg_total)) * 10000) // 1 ) / 100}% packet loss")
+    client_soc.close()
 
-for i in range(msg_count):
-    msg_opcode = my_byte = bytes([0])
-    msg_id = i
-    msg_data = ""
-
-    message = msg_opcode+msg_id+msg_data
-
-    client_soc.sendto(message, server_add)
-    data, server = client_soc.recvfrom(1024)
-
-    print("64 bytes from ", server_ip ," : seq= ", i ," rtt= ", tim)
-
-client_soc.close()
+except OSError as exception:
+    print(exception.strerror)
